6573117251abec42ae11bc0d5ad91d6632a7cff7
Prateek Pandey Chhetri <prateekpanday013@gmail.com>
2026-01-05 22:32:45 +0545

    Roll button Added

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-var randomNumber1 = Math.floor(Math.random()*6 +1)
-var randomDice1= "images/dice"+ randomNumber1+".png"
-document.querySelectorAll("img")[0].setAttribute("src",randomDice1)
+document.querySelector("button").addEventListener("click", function () {
+    
+    var audio=new Audio('sound/dice.mp3');
+            audio.play();
 
-var randomNumber2 = Math.floor(Math.random()*6 +1)
-var randomDice2= "images/dice"+ randomNumber2+".png"
-document.querySelectorAll("img")[1].setAttribute("src",randomDice2)
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1)
+    var randomDice1 = "images/dice" + randomNumber1 + ".png"
 
-if (randomNumber1> randomNumber2){
-    document.querySelector("h1").innerHTML="🏆Player 1 Wins🏆"
-}else if(randomNumber2>randomNumber1){
-    document.querySelector("h1").innerHTML="🏆Player 2 Wins🏆"
-}else{
-    document.querySelector("h1").innerHTML="Draw🟰"
+    document.querySelectorAll("img")[0].setAttribute("src", randomDice1)
+
+    var randomNumber2 = Math.floor(Math.random() * 6 + 1)
+    var randomDice2 = "images/dice" + randomNumber2 + ".png"
+    document.querySelectorAll("img")[1].setAttribute("src", randomDice2)
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🏆Player 1 Wins🏆"
+    } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "🏆Player 2 Wins🏆"
+    } else {
+        document.querySelector("h1").innerHTML = "Draw🟰"
+    }
+
 }
+
+)
